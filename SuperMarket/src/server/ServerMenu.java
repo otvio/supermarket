39dc09eac12dca0f8bc7cf846fe263af755c8cce
8922198 <8922198@6-304-16.grad.usricmc.icmc.usp.br>
2015-06-18 23:12:43 -0300

    Menu do Servidor

--- SuperMarket/src/server/ServerMenu.java
+++ SuperMarket/src/server/ServerMenu.java
@@ -12,10 +12,12 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import static server.Server.CATEGORIES_FILE;
 import static server.Server.DESIRE_FILE;
 import static server.Server.PRODUCTS_FILE;
 import static server.Server.SUPPLIERS_FILE;
 import static server.Server.USERS_FILE;
+import supermarket.entities.Category;
 import supermarket.entities.Product;
 import supermarket.entities.Supplier;
 import supermarket.entities.User;
@@ -24,11 +26,17 @@ import supermarket.entities.User;
 public class ServerMenu {
 
     public void showMenu(){
-        
-        int choice;
+       
         String nameProduct;
         String expirationdate;
+        String nameSupplier;
+        String nameContact;
+        String contacting;
+        String nameCategory;
+        String descriptionCategory;
         int units;
+        int code;
+        int choice;
         double price;
         
         Scanner scanner = new Scanner(System.in);
@@ -39,7 +47,9 @@ public class ServerMenu {
         listProducts = server.BringList();
         
         List <Supplier> listSupplier = new ArrayList<>();
+        listSupplier = getAllSupplier();
         
+        List<Category> listCategory = new ArrayList<>();
         //List <User> listUser = new ArrayList<>();
         
         do{
@@ -47,7 +57,8 @@ public class ServerMenu {
             System.out.println("2 - List all the products");
             System.out.println("3 - Send a notification to the client");
             System.out.println("4 - Registering new Supplier");
-            System.out.println("5 - Quit");
+            System.out.println("5 - Add a new category");
+            System.out.println("6 - Quit");
                 
             choice = scanner.nextInt();
             
@@ -59,6 +70,8 @@ public class ServerMenu {
                         return o1.getCodProduct() < o2.getCodProduct() ? -1 : 1;
                     }
                 });
+                
+                code = (!listProducts.isEmpty()) ? (listProducts.get(listProducts.size() - 1).getCodProduct()+ 1) : 0;  // Código que será adicionado para o usuário
                 
                 System.out.println("\nQuantity of products to add in the store:");
                 units = scanner.nextInt();
@@ -77,12 +90,13 @@ public class ServerMenu {
                 int stockUnits, int orderedUnits, double unitPrice, 
                 String nameProduct, String validity) /*/
                 
-                Product product = new Product(/*fazer funçoes para pegar os codigo*/(listProducts.get(listProducts.size() - 1).getCodProduct() + 1), 0, 0, units, units, price, nameProduct, expirationdate);
+                Product product = new Product(code, 0, 0, units, units, price, nameProduct, expirationdate);
                 
                 product.addFileProduct();
             }
             
             else if(choice == 2){
+               
                 try{
                     listProducts = server.BringList();
                 }
@@ -101,21 +115,54 @@ public class ServerMenu {
             else if(choice == 3){
                 //Send notification to the client
             }
+            
             else if(choice == 4){
                 
-                Collections.sort(listProducts, new Comparator<Product>(){
+                Collections.sort(listSupplier, new Comparator<Supplier>(){
 
                     @Override
-                    public int compare(Product o1, Product o2) {
-                        return o1.getCodProduct() < o2.getCodProduct() ? -1 : 1;
+                    public int compare(Supplier o1, Supplier o2) {
+                        return o1.getCodSupplier() < o2.getCodSupplier() ? -1 : 1;
                     }
                 });
                 
+                code = (!listSupplier.isEmpty()) ? (listSupplier.get(listSupplier.size() - 1).getCodSupplier()+ 1) : 0;  // Código que será adicionado para o usuário
+                
                 System.out.println("\nEnter with the name of the supplier:");
+                nameSupplier = scannerstring.nextLine();
+                
+                System.out.println("Enter with the name of the nameContact:");
+                nameContact = scannerstring.nextLine();
+                
+                System.out.println("Enter with the name of the contacting:");
+                contacting = scannerstring.nextLine();
+                
+                listSupplier.add(new Supplier(code ,nameSupplier, nameContact, contacting));
+                listSupplier.get(listSupplier.size() - 1).addFileSupplier();
             }
             
+            else if(choice == 5){
+                Collections.sort(listCategory, new Comparator<Category>(){
+
+                    @Override
+                    public int compare(Category o1, Category o2) {
+                        return o1.getCodCategory() < o2.getCodCategory() ? -1 : 1;
+                    }
+                });
+                
+                code = (!listCategory.isEmpty()) ? (listCategory.get(listCategory.size() - 1).getCodCategory()+ 1) : 0;  // Código que será adicionado para o usuário
+           
+                System.out.println("\nEnter with the name of the category:");
+                nameCategory = scannerstring.nextLine();
+                System.out.println("Give us a brief description of the category:");
+                descriptionCategory = scannerstring.nextLine();
+                
+                listCategory.add(new Category(code, nameCategory, descriptionCategory));
+                listCategory.get(listSupplier.size() - 1).addFileCategory();
+            }
             
-        }while(choice != 5);
+        }while(choice != 6);
+        
     }
     
     public List <ClientStruct> getDesireList(int codeProduct){
@@ -182,7 +229,7 @@ public class ServerMenu {
         return listUsers;
     }
     
-    public List<Supplier> getAllSuppler(){
+    public List<Supplier> getAllSupplier(){
         
         List<Supplier> listSupplier = new ArrayList<>();
         String line;
@@ -205,4 +252,26 @@ public class ServerMenu {
         return listSupplier;     
     }
     
+    public List<Category> getAllCategory(){
+        List<Category> listCategory = new ArrayList<>();
+        String line;
+        
+        try{
+            BufferedReader buffreader = new BufferedReader(new FileReader(CATEGORIES_FILE));
+            
+            while(buffreader.ready()){
+                line = buffreader.readLine();
+                
+                String[] desires = line.split(",");
+                // pegar o conteudo em arquivo e guardar na lista
+                listCategory.add(new Category(Integer.parseInt(desires[0]), desires[1], desires[2]));
+            }
+        }
+        
+        catch(Exception e){
+            System.out.println("\n::: Can't get the Supplier's list :::");
+        }
+
+        return listCategory;     
+    }
 }
